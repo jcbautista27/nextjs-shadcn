3f4c043ce2b8226261f1a98d30972ba090f8a091
Juan Carlos <jcbautista2701@gmail.com>
2025-04-23 17:54:07 -0500

    shadcn data-table in progress

---
+++ src/app/dashboard/data-table/page.tsx
@@ -0,0 +1,16 @@
+import { payments } from "@/data/payments.data";
+
+async function fetchData() {
+  return payments;
+}
+
+export default async function Page() {
+  const data = await fetchData();
+  return (
+    <div>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
+}
